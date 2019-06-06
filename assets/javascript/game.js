0ef747c5716f0696ef6b71fd2919a33493a60996
Marco Angel <angel2367716@gmail.com>
2019-06-06 00:38:56 -0700

    Designaded possible varialbes for the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//Global variables
+//------------------------------------------------------------
+//Arrays and variables for holding data
+const wordOptions = ["riddikulus", "obliviate", "sectumsempra", "alohomora", "expelliarmus", "accio", "lumos"];
+const selectedWord = " ";
+const lettersInWord = [];
+const numBlanks = 0;
+const blankAndSuccesses = []; //j _ _ _ _ _ _ 
+const wrongLetters = [];
+
+//Game counters
+const winCount = 0;
+const lossCount = 0;
+const guessesLeft = 0;
+
+//Functions (Reusable blocks of code that I will call upon when I need)
+//------------------------------------------------------------
+
+//MAIN PROCESS
+//------------------------------------------------------------
