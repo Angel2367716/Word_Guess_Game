619309c1b8538c4c20bde176ef59c749bfe6e9c8
Marco Angel <angel2367716@gmail.com>
2019-06-07 14:25:07 -0700

    created a system that allows the app to determine if the letter the user type exists on the word that the app wants the user to guess.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,7 @@ let guessesLeft = 9;
 
 //Functions (Reusable blocks of code that I will call upon when I need)
 //------------------------------------------------------------
-function startGame () {
+function startGame() {
     selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)]
     lettersInWord = selectedWord.split("");
     numBlanks = lettersInWord.length;
@@ -25,31 +25,63 @@ function startGame () {
     blankAndSuccesses = [];
 
     //Populate blanks and success with right number of blanks
-    for (i=0; i < numBlanks; i++) {
-        blankAndSuccesses.push ("_");
+    for (i = 0; i < numBlanks; i++) {
+        blankAndSuccesses.push("_");
     }
 
     //Change HTML to reflect round conditions 
-    document.getElementById("wordToGuess").innerHTML = blankAndSuccesses.join (" ");
+    document.getElementById("wordToGuess").innerHTML = blankAndSuccesses.join(" ");
     document.getElementById("numGuesses").innerHTML = guessesLeft;
     document.getElementById("winCounter").innerHTML = winCount;
     document.getElementById("lossCounter").innerHTML = lossCount;
 
     //Testing/Debuging
-    console.log (selectedWord);
-    console.log (lettersInWord);
-    console.log (numBlanks);
-    console.log (blankAndSuccesses);
+    console.log(selectedWord);
+    console.log(lettersInWord);
+    console.log(numBlanks);
+    console.log(blankAndSuccesses);
+}
+
+//Funtion to check the letters match the letters to guess
+function checkLetters(letter) {
+    //check if letter exist at all
+
+    let isLetterInWord = false;
+
+    for (let i = 0; i < numBlanks; i++) {
+        if (selectedWord[i] == letter) {
+            isLetterInWord = true;
+        }
+    }
+    //Check where in word the letter exist, then populate out blanksAndSuccesses array.
+    if (isLetterInWord) {
+        for (let i = 0; i < numBlanks; i++) {
+            if (selectedWord[i] == letter) {
+                blankAndSuccesses[i] = letter;
+            }
+        }
+    }
+    //letter wasn't found
+    else {
+        wrongLetters.push(letter);
+        numGuesses--
+    }
+
+        //Testing and debugging
+        console.log (blankAndSuccesses);
 }
 
 //MAIN PROCESS
 //------------------------------------------------------------
 //Initiates the code for the first time 
-startGame ();
+startGame();
 
 //Register key clicks
-//document.onkeyup = function(event) {
-  //  letter;
-//}
+document.onkeyup = function (event) {
+    let letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    //Testing/debugging
+    console.log(letterGuessed);
+}
 
 //stoped at 15:11 https://www.youtube.com/watch?v=cgdmOR15cn4&feature=youtu.be
