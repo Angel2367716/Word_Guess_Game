0ee15dd4b813ce37a1fa82c1b8e6a578726be59f
Marco Angel <angel2367716@gmail.com>
2019-06-06 18:05:21 -0700

    Added a strat game function to start the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,40 @@
 //------------------------------------------------------------
 //Arrays and variables for holding data
 const wordOptions = ["riddikulus", "obliviate", "sectumsempra", "alohomora", "expelliarmus", "accio", "lumos"];
-const selectedWord = " ";
-const lettersInWord = [];
-const numBlanks = 0;
-const blankAndSuccesses = []; //j _ _ _ _ _ _ 
-const wrongLetters = [];
+let selectedWord = " ";
+let lettersInWord = [];
+let numBlanks = 0;
+let blankAndSuccesses = []; //j _ _ _ _ _ _ 
+let wrongLetters = [];
 
 //Game counters
-const winCount = 0;
-const lossCount = 0;
-const guessesLeft = 0;
+let winCount = 0;
+let lossCount = 0;
+let guessesLeft = 9;
 
 //Functions (Reusable blocks of code that I will call upon when I need)
 //------------------------------------------------------------
+function startGame () {
+    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)]
+    lettersInWord = selectedWord.split(" ");
+    numBlanks = lettersInWord.length;
+    //Reset - each round of the game this will reset the variables
+    guessesLeft = 9;
+    wrongLetters = [];
+    blankAndSuccesses = [];
+
+    //Populate blanks and success with right number of blanks
+    for (i=0; i < numBlanks; i++) {
+        blankAndSuccesses.push ("_");
+    }
+
+    //Testing/Debuging
+    console.log (selectedWord);
+    console.log (lettersInWord);
+    console.log (numBlanks);
+    console.log (blankAndSuccesses);
+}
 
 //MAIN PROCESS
 //------------------------------------------------------------
+startGame ();
