153279b73eb6fe67d63d79a1e69ceb6f8f648346
Marco Angel <angel2367716@gmail.com>
2019-06-07 19:53:10 -0700

    Now everytime the user types a letter if the letter is correct it will show up on the screen. Then if the user wins or loses a message will pop up to let the user know about it.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -64,13 +64,40 @@ function checkLetters(letter) {
     //letter wasn't found
     else {
         wrongLetters.push(letter);
-        numGuesses--
+        guessesLeft--
     }
 
         //Testing and debugging
         console.log (blankAndSuccesses);
 }
 
+function roundComplete () {
+    console.log ("Win count" + winCount + "| Loss count" + lossCount + "| Guesses left" + guessesLeft);
+    //Update the HTML to reflect the most recent count stats
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("wordToGuess").innerHTML = blankAndSuccesses.join(" ");
+    document.getElementById("wrongGuesses").innerHTML = wrongLetters.join (" ");
+
+    //Check if user won
+    if (lettersInWord.toString()== blankAndSuccesses.toString()) {
+        winCount++;
+        alert ("You Won!");
+        
+        //Update the win counter in the HTML
+        document.getElementById("winCounter").innerHTML = winCount;
+        startGame();
+    }
+    //Check if user loss
+    else if (guessesLeft == 0){
+        lossCount++;
+        alert ("You lost");
+
+        //update the lost counter in the HTML
+        document.getElementById("lossCounter").innerHTML = lossCount;
+        
+        startGame();
+    }
+}
 //MAIN PROCESS
 //------------------------------------------------------------
 //Initiates the code for the first time 
@@ -80,6 +107,7 @@ startGame();
 document.onkeyup = function (event) {
     let letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
     checkLetters(letterGuessed);
+    roundComplete();
     //Testing/debugging
     console.log(letterGuessed);
 }
